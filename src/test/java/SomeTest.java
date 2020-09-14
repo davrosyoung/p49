dd6cf3e16bfab138e95c6a1aae6e73efcabdd376
David Young <transitdave@gmail.com>
2020-09-14 20:37:14 +0800

    added tet directory and junit to pom.xml

---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SomeTest {
+  @Test
+  public void testSimpleStuff() {
+    assertEquals("david", "david young".substring(0, 5));
+  }
+}
